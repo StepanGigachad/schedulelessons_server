29036eaf6e337550598e3b367f21bcfe070baead
vlad <bbladik12@gmail.com>
2022-02-16 17:58:05 +0200

    fix for heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio')
 const axios = require('axios')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 const website = 'https://iq.vntu.edu.ua/b04213/curriculum/c_sched4week_m.php?view=g&group_id=5648&f_id=206&s_id=10618'
 
@@ -15,7 +15,7 @@ try {
         let htmlData = []
 
             $(".day, .lesson", data).each(function () {
-                const name = $(this).text().split('\n').map(i => i.replace(/\s+/g, '')).join(' ').split('  ')
+                const name = $(this).text().split('\n').map(i => i.replace(/\s+/g, ' ')).join(' ').split('  ')
                 const day =  $(this).find('div')
                 htmlData.push({
                     text: name.filter(i => i)
